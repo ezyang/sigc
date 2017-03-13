abf61d808964f39df584892ea9f2aa7e465193a0
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-03-13 00:14:06 -0700

    Improve it slightly.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- Main.hs
+++ Main.hs
@@ -4,12 +4,14 @@ import Language.Haskell.Exts
 import Options.Applicative
 import Text.PrettyPrint
 import Data.Set (Set)
+import Data.Char
 import Data.List
+import Control.Monad
 import qualified Data.Set as Set
 
 data Args = Args {
         argSigPath :: FilePath,
-        argModPaths :: [FilePath]
+        argModPaths :: [String]
     } deriving (Show)
 
 argParser :: Parser Args
@@ -17,27 +19,63 @@ argParser = Args
     <$> argument str (metavar "SIG")
     <*> many (argument str (metavar "MOD"))
 
-parseFileIO :: FilePath -> IO (Module SrcSpanInfo)
-parseFileIO = fmap fromParseResult . parseFile
+parseFileIO :: FilePath -> IO (Module SrcSpanInfo, [Comment])
+parseFileIO = fmap fromParseResult . parseFileWithComments defaultParseMode
 
 main :: IO ()
 main = do
     args <- execParser (info argParser mempty)
-    sig <- parseFileIO (argSigPath args)
-    mods <- mapM parseFileIO (argModPaths args)
+    (sig, sig_comments) <- parseFileIO (argSigPath args)
+    let labeledModPaths = map splitModPath (argModPaths args)
+    mods <- forM labeledModPaths $ \(k, v) -> do
+        (mod, _) <- parseFileIO v
+        return (k, mod)
     let sig_exports = moduleExports sig
-        max_len = maximum (map length sig_exports) + 1
-    putStrLn . render $
-        text " " <+> pipe <+> hsep (intersperse pipe (map renderModName mods)) $$
-        hcat (intersperse pipe (replicate (length mods + 1) (text "---"))) $$
-        vcat (map (renderRow max_len mods) sig_exports)
+        max_len = maximum (map (length . snd) sig_exports) + 1
+        header =
+            text (replicate max_len ' ') <> pipe <> hcat (intersperse pipe (map renderModName mods)) $$
+            text (replicate max_len '-') <> pipe <> hcat (intersperse pipe (replicate (length mods) (text "---")))
+    let rows = interleaveExports sig_exports sig_comments
+    putStrLn . render $ vcat (map (renderRow header max_len (map snd mods)) rows)
 
-renderRow :: Int -> [Module l] -> String -> Doc
-renderRow max_len mods sym =
+splitModPath :: String -> (Maybe String, FilePath)
+splitModPath s =
+    case span (/= '=') s of
+        (k, '=':v) -> (Just k, v)
+        _ -> (Nothing, s)
+
+data Row = RowHeader String
+         | Row String
+
+interleaveExports :: [(SrcSpanInfo, String)] -> [Comment] -> [Row]
+interleaveExports [] _ = []
+interleaveExports es [] = map (Row . snd) es
+interleaveExports es'@((le,e):es) cs'@((Comment _ lc c):cs)
+    | lc < srcInfoSpan le
+    = case headerComment c of
+        Just s -> RowHeader s : interleaveExports es' cs
+        Nothing -> interleaveExports es' cs
+    | otherwise
+    = Row e : interleaveExports es cs'
+
+headerComment :: String -> Maybe String
+headerComment s =
+    case span (== '*') (dropWhile isSpace s) of
+        ("*", rs) -> Just (dropWhile isSpace rs)
+        _ -> Nothing
+
+renderRow :: Doc -> Int -> [Module l] -> Row -> Doc
+renderRow header _ _ (RowHeader s) =
+    text "" $$ text "**" <> text s <> text "**" $$ text "" $$ header
+renderRow _header max_len mods (Row sym) =
     text sym $$ nest max_len (pipe <+> hsep (intersperse pipe (map (renderMod sym) mods)))
 
-renderModName :: Module l -> Doc
-renderModName (Module _ (Just (ModuleHead _ (ModuleName _ s) _ _)) _ _ _) = text s
+renderModName :: (Maybe String, Module l) -> Doc
+renderModName (Just s, _)
+    | length s == 1 = text (" " ++ s ++ " ")
+    | length s == 2 = text (" " ++ s)
+    | otherwise = text s
+renderModName (Nothing, (Module _ (Just (ModuleHead _ (ModuleName _ s) _ _)) _ _ _)) = text s
 renderModName _ = text " "
 
 renderMod :: String -> Module l -> Doc
@@ -47,12 +85,12 @@ renderMod sym mod =
         else text " "
   where
     -- Boo, should memo this!
-    avail = Set.fromList (moduleExports mod)
+    avail = Set.fromList (map snd (moduleExports mod))
 
 pipe :: Doc
 pipe = char '|'
 
-moduleExports :: Module l -> [String]
+moduleExports :: Module l -> [(l, String)]
 moduleExports = from_mod
   where
     from_mod (Module _ (Just hd) _ _ _) = from_head hd
@@ -68,5 +106,5 @@ moduleExports = from_mod
     from_export (EModuleContents _ _) = []
     from_qn (UnQual _ n) = [from_n n]
     from_qn _ = []
-    from_n (Ident _ s) = s
-    from_n (Symbol _ s) = s
+    from_n (Ident l s) = (l, s)
+    from_n (Symbol l s) = (l, s)
